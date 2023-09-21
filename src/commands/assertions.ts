b218400ac0ba001354fe678f942f25262e22737b
Adam <33007079+adamperea@users.noreply.github.com>
2023-09-21 11:38:53 +0200

    Add small typo from previous PR (#14)

--- src/commands/assertions.ts
+++ src/commands/assertions.ts
@@ -46,7 +46,7 @@ Cypress.Commands.add('assertInnerTextContains', (dataTestAttributeName: string, 
 });
 
 /**
- * Assert that the current URL contains specified query parameters.
+ * Assert that the current URL contains the specified query parameters.
  * Internally, it relies on `cy.url`.
  *
  * @param params - An array of query parameters to check in the URL.
