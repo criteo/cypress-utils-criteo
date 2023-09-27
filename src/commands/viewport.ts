ed2adff483632de48b63a8484035f53c215aa4be
Antonin KLOPP-TOSSER <antoninklopp.tosser@gmail.com>
2023-09-27 11:22:17 +0200

    Moved models inside commands (#17)
    And added typescript files output into dist
    
    Co-authored-by: a.klopp <a.klopp@criteo.com>

--- src/commands/viewport.ts
+++ src/commands/viewport.ts
@@ -1,6 +1,25 @@
-import { MAX_ANIMATION_TIME, viewportDimensionsByType } from '../constants';
-import { ViewportType } from '../models/enums/viewportType';
+const MAX_ANIMATION_TIME = 250;
 
+interface ViewportDimensions {
+  width: number;
+  height: number;
+}
+
+enum ViewportType {
+  Mobile = 'mobile',
+  Tablet = 'tablet',
+  FlexLayoutMedium = 'fxLayout.md',
+  FlexLayoutLarge = 'fxLayout.lg',
+  HdDesktop = 'hd_desktop',
+}
+
+const viewportDimensionsByType: Readonly<Record<ViewportType, ViewportDimensions>> = {
+  [ViewportType.Mobile]: { width: 375, height: 667 },
+  [ViewportType.Tablet]: { width: 768, height: 1024 },
+  [ViewportType.FlexLayoutMedium]: { width: 1024, height: 768 },
+  [ViewportType.FlexLayoutLarge]: { width: 1280, height: 768 },
+  [ViewportType.HdDesktop]: { width: 1920, height: 1024 },
+};
 /**
  * Set the viewport to the correct resolution
  * Internally, it relies on `cy.viewport`.
@@ -14,4 +33,4 @@ Cypress.Commands.add('changeViewport', (type: ViewportType) => {
   cy.wait(MAX_ANIMATION_TIME);
 });
 
-export {};
+export { ViewportType, ViewportDimensions };
--- src/models/enums/viewportType.ts
+++
@@ -1,7 +0,0 @@
-export enum ViewportType {
-  Mobile = 'mobile',
-  Tablet = 'tablet',
-  FlexLayoutMedium = 'fxLayout.md',
-  FlexLayoutLarge = 'fxLayout.lg',
-  HdDesktop = 'hd_desktop',
-}
